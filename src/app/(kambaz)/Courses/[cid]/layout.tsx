19cdd033a60df11720d3b86106bb0434fed48e87
Zhiye <lizhiye2000@gmail.com>
2025-10-18 16:34:30 -0400

    3.8.4 deployment error fix 2.0

--- src/app/(kambaz)/Courses/[cid]/layout.tsx
+++ src/app/(kambaz)/Courses/[cid]/layout.tsx
@@ -3,14 +3,14 @@ import CourseNavigation from "./Navigation";
 import { notFound } from "next/navigation";
 import { FaAlignJustify } from 'react-icons/fa';
 
-export default function CourseLayout({
+export default async function CourseLayout({
   children,
   params,
 }: {
   children: React.ReactNode;
   params: { cid: string };
 }) {
-  const { cid } = params;
+  const { cid } = await params;
   const course = courses.find((c) => c._id === cid);
   if (!course) notFound();
 
